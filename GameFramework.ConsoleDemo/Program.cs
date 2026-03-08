18e57a3266bbe5aaf591b95f53f629f64c1aed79
antonwessel <anton.wessel01@outlook.com>
2026-03-08 12:04:30 +0100

    add singleton logger

--- GameFramework.ConsoleDemo/Program.cs
+++ GameFramework.ConsoleDemo/Program.cs
@@ -1,5 +1,9 @@
 ﻿using GameFramework.Configuration;
 using GameFramework.Models;
+using GameFramework.Logging;
+using System.Diagnostics;
+
+// Testing XML-configuration
 
 GameConfiguration config = GameConfigurationLoader.Load("Configuration/GameConfiguration.xml");
 
@@ -8,3 +12,15 @@ World world = new(config.MaxX, config.MaxY);
 Console.WriteLine($"World MaxX: {world.MaxX}");
 Console.WriteLine($"World MaxY: {world.MaxY}");
 Console.WriteLine($"Difficulty: {config.Difficulty}");
+
+// Testing logging
+
+TextWriterTraceListener listener = new(Console.Out);
+
+MyLogger.Instance.AddListener(listener);
+MyLogger.Instance.Log("test log message");
+Trace.Flush();
+
+MyLogger.Instance.RemoveListener(listener);
+MyLogger.Instance.Log("this should not appear");
+Trace.Flush();
---
+++ GameFramework/Logging/MyLogger.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace GameFramework.Logging;
+
+public sealed class MyLogger
+{
+    public static MyLogger Instance { get; } = new MyLogger();
+
+    private MyLogger()
+    {
+
+    }
+
+    public void Log(string message)
+    {
+        Trace.WriteLine(message);
+    }
+
+    public void AddListener(TraceListener listener)
+    {
+        ArgumentNullException.ThrowIfNull(listener);
+        Trace.Listeners.Add(listener);
+    }
+
+    public void RemoveListener(TraceListener listener)
+    {
+        ArgumentNullException.ThrowIfNull(listener);
+        Trace.Listeners.Remove(listener);
+    }
+}
